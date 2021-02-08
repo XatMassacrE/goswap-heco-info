175671de10c023bbe0afc965e6179a593d6bb46b
marui <1016595756@qq.com>
2021-02-08 19:43:28 +0800

    等待效果

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -2,37 +2,61 @@ import React from 'react'
 import styled, { css, keyframes } from 'styled-components'
 
 const rotate = keyframes`
-  0% { transform: scale(1); }
-  60% { transform: scale(1.1); }
-  100% { transform: scale(1); }
+0% {
+  -webkit-transform:perspective(220px);
+}
+60% {
+  -webkit-transform:perspective(220px) rotateY(180deg);
+}
+100% {
+  -webkit-transform:perspective(220px) rotateY(0);
+}
 `
 
 const Loader = styled.div`
   pointer-events: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-  animation: ${rotate} 800ms linear infinite;
-  & > * {
-    width: 72px;
+  position:absolute;
+  width:60px;
+  height:60px;
+  margin: 300px auto;
+top:80%;
+left:50%;
+ @media (max-width: 835px) {
+    left:44%;
   }
-  ${props =>
-    props.fill && !props.height
-      ? css`
-          height: 100vh;
-        `
-      : css`
-          height: 180px;
-        `}
+  backface-visibility:hidden;
+  z-index:2;
+  animation: ${rotate} 3000ms ease-in-out infinite;
 `
 
+const Loadera = styled.div`
+  pointer-events: none;
+  position:absolute;
+  margin: 300px auto;
+top:80%;
+left:50%;
+@media (max-width: 835px) {
+ left:44%;
+}
+  z-index:1;
+  animation: ${rotate} 3000ms ease-in-out infinite;
+`
+const Rotate = styled.div`
+width:100%;
+height:100%;
+position:relative;
+`
 const LocalLoader = ({ fill }) => {
   return (
-    <Loader fill={fill}>
-      <img src={require('../../assets/logo.png')} alt="loading-icon" />
-    </Loader>
+    <Rotate>
+      <Loader>
+        <img src={require('../../assets/logo.png')} alt="loading-icon" width="60" height="60" />
+      </Loader>
+      <Loadera>
+        <img src={require('../../assets/logoa.png')} alt="loading-icon" width="60" height="60" />
+      </Loadera>
+    </Rotate>
+
   )
 }
 
